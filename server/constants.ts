3872e3f900a3198eb163986cee3cccd4739eec09
Desmond Tuiyot <dtuiyot@gmail.com>
2023-11-24 21:36:12 -0700

    feat: add typegraphql support

---
+++ server/constants.ts
@@ -0,0 +1,2 @@
+export const SERVER_PORT=4000
+export const SERVER_GQL_PATH='/graphql'
--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,66 @@
+import "reflect-metadata"
 import express from 'express'
-import { PrismaClient } from '@prisma/client'
+import { ApolloServer } from '@apollo/server'
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import { expressMiddleware } from '@apollo/server/express4';
+import http from 'http'
+import { buildSchema } from 'type-graphql'
+import { resolvers } from '@generated/type-graphql'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
-const app = express()
-const port = 4000
+import { SERVER_PORT, SERVER_GQL_PATH } from './constants'
 
-app.use(express.json())
+const init = async () => {
+  const PORT = process.env.PORT || SERVER_PORT
+  const app = express()
+  const prisma = new PrismaClient()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  // TODO: define cors when we have a frontend - https://www.apollographql.com/docs/apollo-server/security/cors/#specifying-origins
+  // TODO: define cookie parser when we have a frontend
+  
+  app.use(express.json())
 
-app.get('/moves', async (req, res): Promise<void> => {
-  const moves = await prisma.danceMove.findMany()
-  res.send(moves)
-})
+  const httpServer = http.createServer(app)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  // Apollo server
+  const createSchema = async () => {
+    return await buildSchema({
+      resolvers,
+      emitSchemaFile: true,
+      validate: false,
+    })
+  }
+
+  const schema = await createSchema()
+  
+  const server = new ApolloServer({
+    schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
+  })
+
+  await server.start()
+
+  app.use(
+    '/graphql',
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ prisma, token: req.headers.token })
+    }),
+  )
+
+  app.get('/', (_, res) => {
+    res.send(`Perceuse API - <a href="${String(SERVER_GQL_PATH)}">To Playground</a>`)
+  })
+
+  app.get('/health-check', (_, res) => {
+    res.type('application/health+json')
+    res.json({ status: 'pass' })
+  })
+
+  await new Promise<void>(resolve => httpServer.listen({ port: PORT }, resolve))
+
+  console.log(`boss-graphql-api live at http://localhost:${PORT}/graphql`)
+  console.log(`Try your health check at: http://localhost:${PORT}/health-check`)
+}
+
+init().catch(error => console.log(error))
